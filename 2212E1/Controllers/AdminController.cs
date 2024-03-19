cf5643271c0bc0acdf26f76d5c96ec8b31ce217b
AMAAN-DURRANI <faizanaptech9998@gmail.com>
2024-03-19 16:50:17 +0500

    Admin Panel Added

---
+++ 2212E1/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2212E1.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
